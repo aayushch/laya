b9fde2affa39d4c5ce17d03c73c6f544dc811fb1
Aayush Chawla <aayush.chawla@groundlabs.com>
2026-04-17 10:52:35 +0800

    Add missing file

---
+++ ui/src/lib/stores/accessibleColors.ts
@@ -0,0 +1,16 @@
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+const initial: boolean = browser
+	? (localStorage.getItem('laya-accessible-colors') ?? 'false') === 'true'
+	: false;
+
+const { subscribe, set } = writable<boolean>(initial);
+
+export const accessibleColors = {
+	subscribe,
+	set(value: boolean) {
+		set(value);
+		if (browser) localStorage.setItem('laya-accessible-colors', String(value));
+	}
+};
